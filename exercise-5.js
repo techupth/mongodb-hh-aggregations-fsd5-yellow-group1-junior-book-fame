20260e78bbd4ed1b1b6293f029474ae2c9b78218
junior <jr.foto@hotmail.com>
2024-01-11 20:35:35 +0700

    junior-ex

--- exercise-5.js
+++ exercise-5.js
@@ -1,1 +1,18 @@
 // Paste the complete MQL here
+db.pizzaOrders.aggregate([
+  {
+    $group: {
+      _id: {
+        year: { $year: "$created_at" },
+        month: { $month: "$created_at" },
+      },
+      total_sales: { $sum: "$total_price" },
+    },
+  },
+  {
+    $sort: {
+      "_id.year": -1,
+      "_id.month": -1,
+    },
+  },
+]);
--- exercise-6.js
+++ exercise-6.js
@@ -1,1 +1,17 @@
 // Paste the complete MQL here
+db.pizzaOrders.aggregate([
+  {
+    $match: {
+      created_at: {
+        $gte: ISODate("2021-07-01T00:00:00.000Z"),
+        $lt: ISODate("2021-08-01T00:00:00.000Z"),
+      },
+    },
+  },
+  {
+    $group: {
+      _id: "month:7",
+      total_sales: { $sum: "$total_price" },
+    },
+  },
+]);
